d615a498f38ebbddbd4720854c9d33ab01dc1cbe
satyam kumar <satyam@apiria.com>
2018-04-24 19:04:03 +0530

    add header, register,login and student component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { StudentLoginComponent } from './student-login/student-login.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RegisterComponent,
+    LoginComponent,
+    StudentLoginComponent
   ],
   imports: [
     BrowserModule
